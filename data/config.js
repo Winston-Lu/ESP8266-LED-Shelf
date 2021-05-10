9d20a636b993f2060deb91f8d037d28703600f83
Winston.Lu <wlu2268@gmail.com>
2021-05-09 21:59:41 -0400

    Remade site for mobile support

---
+++ data/config.js
@@ -0,0 +1,6 @@
+// ------------------------------ START OF CONFIGURATION ------------------------------ //
+const enable24HR = false;
+//if you have your own dimensions, set it here. Otherwise, leave it blank for auto-configuration
+let HEIGHT = 0;
+let WIDTH = 0;
+// ------------------------------ END OF CONFIGURATION ------------------------------ //
