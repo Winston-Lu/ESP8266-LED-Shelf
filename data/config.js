88159a7b5946345232e836ddd820711424492447
Winston Lu <33874247+Winston-Lu@users.noreply.github.com>
2022-03-10 20:55:16 -0500

    Update config.js
    reverted default back to 12hr

--- data/config.js
+++ data/config.js
@@ -1,5 +1,5 @@
 // ------------------------------ START OF CONFIGURATION ------------------------------ //
-const enable24HR = true;
+const enable24HR = false;
 //if you have your own dimensions, set it here. Otherwise, leave it blank for auto-configuration
 let HEIGHT = 0;
 let WIDTH = 0;
